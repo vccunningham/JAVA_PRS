612fa84dcc7cce1ef327d063d4d763960d720dd1
vcunningham <vaughn.cunningham@healthcarousel.com>
2022-05-25 21:57:53 -0400

    first commit

---
+++ src/main/java/com/cunningham/vaughn/prs/PrsApplication.java
@@ -0,0 +1,13 @@
+package com.cunningham.vaughn.prs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cunningham/vaughn/prs/PrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunningham.vaughn.prs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
